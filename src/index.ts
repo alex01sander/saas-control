d6f61e529f7c025b10c230ec4c34797a4ad6e9d3
Alex Britto <alexsander01@hotmail.com.br>
2026-02-25 08:09:44 -0300

    chore: configure prisma v7 and initial migration

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
+import "dotenv/config";
 import express from "express";
-import type { Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
+import router from "./routes.js";
+import AppError from "./errors/AppError.js";
 
 const app = express();
 const port = process.env["PORT"] || 3000;
 
 app.use(express.json());
+app.use(router)
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "SaaS Control API is running!" });
+app.use((error: AppError, req: Request, res: Response, next: NextFunction) => {
+    console.error(error);
+
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal server error",
+    });
 });
 
 app.listen(port, () => {
