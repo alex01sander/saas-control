0c1e62b3322cda7022caa443805979f2765e823f
Alex Britto <alexsander01@hotmail.com.br>
2026-03-18 17:57:56 -0300

    refactor(routes): link real pages and remove placeholders

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,28 +1,22 @@
 import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import { RouteGuard } from "./RouteGuard";
 
+import { LoginPage } from "../pages/Login";
+import { RegisterPage } from "../pages/Register";
+
+const DashboardPage = () => <div>Dashboard (Em breve - Precisa de Layout)</div>;
+
 export function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<RouteGuard />}>
-                    <Route path="/login" element={<h1>Login Page</h1>} />
-                    <Route path="/register" element={<h1>Register Page</h1>} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
                 </Route>
 
                 <Route element={<RouteGuard isPrivate />}>
-                    <Route path="/dashboard" element={<h1>Dashboard</h1>} />
-                    <Route path="/profile" element={<h1>Profile</h1>} />
-                    <Route path="/plans" element={<h1>Escolha seu Plano</h1>} />
-                </Route>
-
-                <Route
-                    element={<RouteGuard isPrivate isSubscriptionRequired />}
-                >
-                    <Route
-                        path="/premium-content"
-                        element={<h1>Conteúdo Exclusivo</h1>}
-                    />
+                    <Route path="/dashboard" element={<DashboardPage />} />
                 </Route>
 
                 <Route path="*" element={<Navigate to="/dashboard" />} />
