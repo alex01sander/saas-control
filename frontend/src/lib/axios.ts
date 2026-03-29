178b30a56fd142b715a323fbe18907820be31806
Alex Britto <alexsander01@hotmail.com.br>
2026-03-29 20:11:13 -0300

    fix: use environment variable for API baseURL

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://localhost:3000",
+    // Se a variável de ambiente existir, usa ela. Senão, cai no localhost (para quando você estiver desenvolvendo)
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
 });
 
 api.interceptors.request.use((config) => {
